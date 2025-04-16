4e8b743c0cd68b20fcb028cfc384df4c514050af
Hrishikesh Kokate <hrishi5200@gmail.com>
2025-04-16 20:29:34 +0800

    initial commit

---
+++ packages/nextjs/next.config.ts
@@ -0,0 +1,25 @@
+import { cwd } from 'node:process'
+import { join } from 'node:path'
+import type { NextConfig } from 'next'
+import { readFileSync } from 'node:fs'
+
+const nextConfig: NextConfig = {
+  async redirects() {
+    return readFileSync(
+      join(cwd(), 'public', '_redirects'),
+      'utf-8'
+    )
+    .split('\n')
+    .filter(l => l.trim().length)
+    .map(r => {
+      const [source, destination] = r.split(' ')
+      return {
+        destination: destination.trim(),
+        permanent: true,
+        source: source.trim()
+      }
+    })
+  }
+}
+
+export default nextConfig
